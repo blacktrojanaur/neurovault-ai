0e35233076bd89f1d7ccbb390571e39be25f1f3e
Ambar Bansal <ambarbansal2@gmail.com>
2026-01-25 04:49:07 +0530

    fix wagmi + nextjs vercel build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: "standalone",
+  output: "export",
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
